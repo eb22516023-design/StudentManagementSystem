b5b01f0e333dc898147990ec5c302bf78fd486e9
eb22516023-design <eb22516023@utg.edu.gm>
2026-01-12 02:30:28 +0000

    I remove the comment

--- src/main/java/com/utg/studentmanagement/controller/StudentController.java
+++ src/main/java/com/utg/studentmanagement/controller/StudentController.java
@@ -1,39 +1,3 @@
-//package com.utg.studentmanagement.controller;
-//
-//import com.utg.studentmanagement.entity.Student;
-//import com.utg.studentmanagement.repository.StudentRepository;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/students")
-//public class StudentController {
-//
-//        private final StudentRepository studentRepository;
-//
-//        public StudentController(StudentRepository studentRepository) {
-//            this.studentRepository = studentRepository;
-//        }
-//
-//        @PostMapping
-//        public Student addStudent(@RequestBody Student student) {
-//            System.out.println("POST /students CALLED");
-//            System.out.println(student.getName());
-//            System.out.println(student.getEmail());
-//            System.out.println(student.getCourse());
-//            return studentRepository.save(student);
-//        }
-//
-//        @GetMapping
-//        public List<Student> getAllStudents() {
-//            return studentRepository.findAll();
-//        }
-
-
-
-//
-
 package com.utg.studentmanagement.controller;
 
 import com.utg.studentmanagement.entity.Student;
